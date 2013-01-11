cfa5117b1ac1225ae2d8702156aa8d0ed10c6240
Satoshi Egi <egisatoshi@gmail.com>
2013-01-11 13:55:23 +0900

    updating Types.hs

--- hs-src/Language/Egison/Types.hs
+++ hs-src/Language/Egison/Types.hs
@@ -22,60 +22,123 @@ data EgisonTypeExpr =
   | IntegerTypeExpr
   | FloatTypeExpr
   | TypeTypeExpr
+  | ClassTypeExpr
+  | PatternTypeExpr
 
   | TupleTypeExpr [EgisonTypeExpr]
   | CollectionTypeExpr EgisonTypeExpr
   | FunTypeExpr EgisonTypeExpr EgisonTypeExpr
+  | AppTypeExpr EgisonTypeExpr EgisonTypeExpr
 
   | PatVarTypeExpr String
   | VarTypeExpr String
         
+data EgisonType =
+    CharType
+  | StringType
+  | BoolType
+  | IntegerType
+  | FloatType
+  | TypeType
+  | ClassType
+  | PatternType
+
+  | TupleType [EgisonType]
+  | CollectionType EgisonType
+  | FunType EgisonType EgisonType
+  | AppType EgisonType EgisonType
+
+  | PatVarType String
+  | VarType String
+        
 data EgisonExpr =
-    CharExpr Char
-  | StringExpr String
-  | BoolExpr Bool
-  | IntegerExpr Integer
-  | FloatExpr Double
-  | PatVarExpr String [EgisonExpr]
-  | VarExpr String [EgisonExpr]
-  | SymbolExpr String
-  | PatVarOmitExpr String [EgisonExpr]
-  | VarOmitExpr String [EgisonExpr]
+    CharExpr Char (Maybe EgisonTypeExpr)
+  | StringExpr String (Maybe EgisonTypeExpr)
+  | BoolExpr Bool (Maybe EgisonTypeExpr)
+  | IntegerExpr Integer (Maybe EgisonTypeExpr)
+  | FloatExpr Double (Maybe EgisonTypeExpr)
+  | PatVarExpr String [EgisonExpr] (Maybe EgisonTypeExpr)
+  | VarExpr String [EgisonExpr] (Maybe EgisonTypeExpr)
+  | SymbolExpr String (Maybe EgisonTypeExpr)
+  | PatVarOmitExpr String [EgisonExpr] (Maybe EgisonTypeExpr)
+  | VarOmitExpr String [EgisonExpr] (Maybe EgisonTypeExpr)
 
-  | WildCardExpr
-  | ValuePatExpr EgisonExpr
-  | CutPatExpr EgisonExpr
-  | NotPatExpr EgisonExpr
-  | AndPatExpr [EgisonExpr]
-  | OrPatExpr [EgisonExpr]
-  | PredPatExpr EgisonExpr [EgisonExpr]
+  | WildCardExpr (Maybe EgisonTypeExpr)
+  | ValuePatExpr EgisonExpr (Maybe EgisonTypeExpr)
+  | CutPatExpr EgisonExpr (Maybe EgisonTypeExpr)
+  | NotPatExpr EgisonExpr (Maybe EgisonTypeExpr)
+  | AndPatExpr [EgisonExpr] (Maybe EgisonTypeExpr)
+  | OrPatExpr [EgisonExpr] (Maybe EgisonTypeExpr)
+  | PredPatExpr EgisonExpr [EgisonExpr] (Maybe EgisonTypeExpr)
 
-  | InductiveDataExpr String [EgisonExpr]
-  | TupleExpr [EgisonExpr]
-  | CollectionExpr [InnerExpr]
-  | FuncExpr EgisonExpr EgisonExpr
+  | InductiveDataExpr String [EgisonExpr] (Maybe EgisonTypeExpr)
+  | TupleExpr [EgisonExpr] (Maybe EgisonTypeExpr)
+  | CollectionExpr [InnerExpr] (Maybe EgisonTypeExpr)
+  | FuncExpr EgisonExpr EgisonExpr (Maybe EgisonTypeExpr)
 
-  | IfExpr EgisonExpr EgisonExpr EgisonExpr
-  | LetExpr Bindings EgisonExpr
-  | LetRecExpr RecursiveBindings EgisonExpr
+  | IfExpr EgisonExpr EgisonExpr EgisonExpr (Maybe EgisonTypeExpr)
+  | LetExpr Bindings EgisonExpr (Maybe EgisonTypeExpr)
+  | LetRecExpr RecursiveBindings EgisonExpr (Maybe EgisonTypeExpr)
 
-  | TypeExpr TypeInfoExpr
-  | Class ClassInfoExpr
+  | TypeExpr TypeInfoExpr (Maybe EgisonTypeExpr)
+  | ClassExpr ClassInfoExpr (Maybe EgisonTypeExpr)
 
-  | MatchExpr EgisonExpr EgisonExpr [MatchClause]
-  | MatchAllExpr EgisonExpr EgisonExpr MatchClause
+  | MatchExpr EgisonExpr EgisonExpr [MatchClause] (Maybe EgisonTypeExpr)
+  | MatchAllExpr EgisonExpr EgisonExpr MatchClause (Maybe EgisonTypeExpr)
 
-  | LoopExpr String String EgisonExpr EgisonExpr EgisonExpr
-  | DoExpr Bindings EgisonExpr
+  | LoopExpr String String EgisonExpr EgisonExpr EgisonExpr (Maybe EgisonTypeExpr)
+  | DoExpr Bindings EgisonExpr (Maybe EgisonTypeExpr)
 
-  | ApplyExpr EgisonExpr EgisonExpr
+  | ApplyExpr EgisonExpr EgisonExpr (Maybe EgisonTypeExpr)
 
-  | SomethingExpr
-  | UndefinedExpr
+  | SomethingExpr (Maybe EgisonTypeExpr)
+  | UndefinedExpr (Maybe EgisonTypeExpr)
  deriving (Show)
- 
-type ArgsExpr = Args
-               
+
+data EgisonTypedExpr =
+    CharTypedExpr Char EgisonType
+  | StringTypedExpr String EgisonType
+  | BoolTypedExpr Bool EgisonType
+  | IntegerTypedExpr Integer EgisonType
+  | FloatTypedExpr Double EgisonType
+  | PatVarTypedExpr String [EgisonTypedExpr] EgisonType
+  | VarTypedExpr String [EgisonTypedExpr] EgisonType
+  | SymbolTypedExpr String EgisonType
+  | PatVarOmitTypedExpr String [EgisonTypedExpr] EgisonType
+  | VarOmitTypedExpr String [EgisonTypedExpr] EgisonType
+
+  | WildCardTypedExpr EgisonType
+  | ValuePatTypedExpr EgisonTypedExpr EgisonType
+  | CutPatTypedExpr EgisonTypedExpr EgisonType
+  | NotPatTypedExpr EgisonTypedExpr EgisonType
+  | AndPatTypedExpr [EgisonTypedExpr] EgisonType
+  | OrPatTypedExpr [EgisonTypedExpr] EgisonType
+  | PredPatTypedExpr EgisonTypedExpr [EgisonTypedExpr] EgisonType
+
+  | InductiveDataTypedExpr String [EgisonTypedExpr] EgisonType
+  | TupleTypedExpr [EgisonTypedExpr] EgisonType
+  | CollectionTypedExpr [InnerTypedExpr] EgisonType
+  | FuncTypedExpr EgisonTypedExpr EgisonTypedExpr EgisonType
+
+  | IfTypedExpr EgisonTypedExpr EgisonTypedExpr EgisonTypedExpr EgisonType
+  | LetTypedExpr Bindings EgisonTypedExpr EgisonType
+  | LetRecTypedExpr RecursiveBindings EgisonTypedExpr EgisonType
+
+  | TypeTypedExpr TypeInfoExpr EgisonType
+  | ClassTypedExpr ClassInfoExpr EgisonType
+
+  | MatchTypedExpr EgisonTypedExpr EgisonTypedExpr [MatchClause] EgisonType
+  | MatchAllTypedExpr EgisonTypedExpr EgisonTypedExpr MatchClause EgisonType
+
+  | LoopTypedExpr String String EgisonTypedExpr EgisonTypedExpr EgisonTypedExpr EgisonType
+  | DoTypedExpr Bindings EgisonTypedExpr EgisonType
+
+  | ApplyTypedExpr EgisonTypedExpr EgisonTypedExpr EgisonType
+
+  | SomethingTypedExpr EgisonType
+  | UndefinedTypedExpr EgisonType
+ deriving (Show)
+
 type MatchClause = (EgisonExpr, EgisonExpr)
 
 data PrimitivePatPattern =
@@ -150,6 +213,7 @@ data EgisonVal =
   | Tuple [EgisonVal]
   | Collection [EgisonVal]
   | Type TypeInfo
+  | Class ClassInfo
   | Func ObjectRef EgisonExpr Env
   | PrimitiveFunc ([EgisonVal] -> ThrowsError EgisonVal)
   | IOFunc ([EgisonVal] -> IOThrowsError EgisonVal)
@@ -175,6 +239,7 @@ data InnerObject =
   | ISubCollection ObjectRef
 
 type TypeInfo = [(PrimitivePatPattern, ObjectRef, [(Env, PrimitivePattern, EgisonExpr)])]
+type ClassInfo = [(String, EgisonType)]
 
 --
 -- Internal Data
